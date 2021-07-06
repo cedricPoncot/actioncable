bac5139dff2460cc49d333407cc4762c1c30cdc4
cedricPoncot <cedric.poncot@univ-lyon2.fr>
2021-07-06 17:08:01 +0200

    Adding some docs

--- README.md
+++ README.md
@@ -1,19 +1,12 @@
 # Actioncable
 
-To start your Phoenix server:
+## Installation
 
-  * Install dependencies with `mix deps.get`
-  * Install Node.js dependencies with `cd assets && npm install`
-  * Start Phoenix endpoint with `mix phx.server`
+Add the :actioncable dependency to your mix.exs file
 
-Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
-
-Ready to run in production? Please [check our deployment guides](https://hexdocs.pm/phoenix/deployment.html).
-
-## Learn more
-
-  * Official website: http://www.phoenixframework.org/
-  * Guides: https://hexdocs.pm/phoenix/overview.html
-  * Docs: https://hexdocs.pm/phoenix
-  * Mailing list: http://groups.google.com/group/phoenix-talk
-  * Source: https://github.com/phoenixframework/phoenix
+```
+defp deps() do
+  [
+    {:actioncable, "~> 0.1.0"}
+  ]
+```
--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule Actioncable.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      description: description,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -30,6 +31,13 @@ defmodule Actioncable.MixProject do
     """
   end
 
+  defp package do
+    [
+      links: %{"GitHub" => "https://github.com/cedricPoncot/actioncable"},
+      licenses: ["MIT"]
+    ]
+  end
+
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
@@ -47,7 +55,8 @@ defmodule Actioncable.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:redix, "~> 1.1.3"},
-      {:poison, ">= 0.0.0"}
+      {:poison, ">= 0.0.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
     ]
   end
 end
