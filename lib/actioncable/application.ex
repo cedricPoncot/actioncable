e5111c7cd770a08e2c2fdb940571feef7b2a4ef8
cedricPoncot <cedric.poncot@univ-lyon2.fr>
2021-07-09 10:54:36 +0200

    Add docs

--- lib/actioncable/application.ex
+++ lib/actioncable/application.ex
@@ -1,7 +1,12 @@
 defmodule Actioncable.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
-  @moduledoc false
+  @moduledoc """
+  Implementation of Actioncable server. This is compatible with Actioncable JS client.
+  This implementation needs REDIS to store every websocket pid (value) into corresponding channel (key).
+  For complete setup of this actioncable server, please check complete tutorial in the github page.
+  Do not hesitate to write feedback / issue, this is a first version. Thanks.
+  """
 
   use Application
 
@@ -16,7 +21,7 @@ defmodule Actioncable.Application do
       }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
+    # See https://hexdocs.pm/elixir/Actioncable.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Actioncable.Supervisor]
     Supervisor.start_link(children, opts)
